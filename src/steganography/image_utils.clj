1d217d8804998b31c39d63773150dc33d0b35682
Milica <marinkovicm240@gmail.com>
2024-04-25 14:33:56 +0200

    Created namespace for loading and saving images

---
+++ src/steganography/image_utils.clj
@@ -0,0 +1,27 @@
+(ns steganography.image-utils
+  (:import (javax.imageio ImageIO)
+           (java.io File)))
+
+(defn load-image
+  "Loads an image from a file and returns the image object. Returns nil if an error occurs."
+  [file-path]
+  (try
+    (ImageIO/read (File. file-path))
+    (catch Exception e
+      (println "Error loading image:" (.getMessage e))
+      nil)))
+
+(defn- get-executing-directory
+  "Returns the path of the current working directory."
+  []
+  (System/getProperty "user.dir"))
+
+(defn- save-image
+  "Saves an image to a file in PNG format. Returns true if saving is successful, otherwise false."
+  [image-data file-path]
+  (let [output-file (File. (str (get-executing-directory) "/" file-path ".png"))]
+    (try
+      (ImageIO/write image-data "png" output-file)
+      (catch Exception e
+        (println "Error saving image:" (.getMessage e))
+        false))))
